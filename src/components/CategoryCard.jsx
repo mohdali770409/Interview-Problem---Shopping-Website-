180c87afc0122435da3dfb99ed80fe9b8635592c
Mohd Ali <mohdali770409@gmail.com>
2024-06-28 00:58:49 +0530

    fixed categories

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const CategoryCard = ({ category, link, url }) => {
   return (
     <div>
-      <div className="h-[300px] w-[300px] relative transition duration-300 hover:scale-105 border  rounded-lg">
+      <div className="h-[300px] w-[300px] relative transition duration-300 hover:scale-105 border  rounded-lg ">
         <Link to={`/category/${link}`}>
           {" "}
           <img
@@ -12,7 +12,7 @@ const CategoryCard = ({ category, link, url }) => {
             alt=""
             className="w-full h-full object-cover rounded-lg "
           />
-          <p className="absolute bottom-1 right-1 font-bold text-stone-300">
+          <p className="absolute bottom-1 right-1 font-bold text-stone-300 ">
             {category}
           </p>
         </Link>
